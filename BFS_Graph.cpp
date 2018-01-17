bdc692f34171e9ceb8d3332775d2efbf70a68611
Vijay Manikpuri <vijaydas.mp@gmail.com>
2018-01-17 10:24:41 +0530

    Create BFS_search_graph.c
    bfs in graph

---
+++ BFS_Graph.cpp
@@ -0,0 +1,63 @@
+// Program to print BFS traversal from a given
+// source vertex. BFS(int s) traverses vertices 
+// reachable from s.
+#include<iostream>
+#include <list>
+ 
+using namespace std;
+
+class Graph
+{
+   int V;
+   list<int> *adj;
+   
+   public:
+   Graph (int v);
+   // add edges u - v // undoretced graph
+   void addedge(int u, int v);
+   void BFS(int s);
+};
+
+Graph::Graph(int v)
+{
+  V = v;
+  adj = new list<int> adj[v];
+}
+
+Graph::addedge(int u, int v)
+{
+  // directed graph
+   adj[u].push_back[v];
+   // for undirected graph we should be creating a back edege also
+   // adj[v].push_back[u];
+}
+
+Graph::BFS(int s)
+{
+  bool *visited = new bool[V];
+  for (int i =0; i< V; i++)
+        visited[i] = false;
+        
+  Queue<int> q;
+  q.push(s);
+  
+  visited[s] = true;
+  
+  list<int>::iterator i;
+  
+  while(!q.empty())
+  {
+    s = q.front();
+    cout<<q.front();
+    q.pop();
+  
+    for( i = adj[s].begin(); i < adj[s].end(); i++)
+    {
+       if(!visited[*i]) 
+       {
+          q.push(*i);
+          visited[*i] = true;
+       } // if
+    } // for
+  }//while
+} // end method
