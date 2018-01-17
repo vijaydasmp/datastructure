1dd41be90b243b92f30cd7affee91a33216bc2d5
Vijay Manikpuri <vijaydas.mp@gmail.com>
2018-01-17 10:27:33 +0530

    update BFS_Graph.cpp

--- BFS_Graph.cpp
+++ BFS_Graph.cpp
@@ -61,3 +61,20 @@ Graph::BFS(int s)
     } // for
   }//while
 } // end method
+
+
+int main()
+{
+  Graph g(4);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(2, 0);
+    g.addEdge(2, 3);
+    g.addEdge(3, 3);
+ 
+    cout << "Following is Breadth First Traversal "
+         << "(starting from vertex 2) \n";
+    g.BFS(2);
+    return 0;
+}
