89d32aa5b80f453e3c6c14c5683a65103066a23a
Vijay Manikpuri <vijaydas.mp@gmail.com>
2018-01-17 21:18:25 +0530

    Update topological_sorting.cpp

--- topological_sorting.cpp
+++ topological_sorting.cpp
@@ -52,3 +52,17 @@ Graph::topo()
         stk.pop()
     }
 }
+
+int main()
+{
+     Graph g(5);g.addEdge(5, 2);
+    g.addEdge(5, 0);
+    g.addEdge(4, 0);
+    g.addEdge(4, 1);
+    g.addEdge(2, 3);
+    g.addEdge(3, 1);
+ 
+    cout << "Following is a Topological Sort of the given graph n";
+    
+     topo();
+}
