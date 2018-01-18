c16598d5836a63d6b9b7cc6ced39ca65e3e382c0
Vijay Manikpuri <vijaydas.mp@gmail.com>
2018-01-18 11:15:20 +0530

    Update detect_cycle_undirected.cpp

--- detect_cycle_undirected.cpp
+++ detect_cycle_undirected.cpp
@@ -1,3 +1,56 @@
-class Graph{
+class Graph
+{
 
+  private:
+    int V;
+    list<int> *adj
+    void has_cycle_util(int s, bool visited[]);  
+  public:
+   Graph(int v);
+   void addEdge(int u, int v);
+   bool has_cycle();
+};
+
+Graph::Graph(int v)
+{
+   V = v;
+   adj = new list<int>[V];
+}
+
+Graph::addEdge(int u, int v)
+{
+  adj[u].push_back(v);
+}
+
+Grap::has_cycle(int s)
+{
+  bool *visited = new bool[V];
+  
+  for (int i = 0; i< V; i++)
+        visited[i] = false;
+  
+  list<int>::iterator i;
+    
+  for ( i = adj[s].begin(); i != adj[s].end())
+  {
+     if (!visited[*i])
+        has_cycle_util(*i, visited)
+       
+  }
+  
+}
+int main()
+{
+  Graph g(4);
+  g.addedge(0,1)
+  // ...
+  // ....
+  if (g.has_cycle())
+  {
+    cout<<"Has Cycle";
+  }
+  else
+  {
+    cout<<"Dont Have Cycle";
+  }   
 }
