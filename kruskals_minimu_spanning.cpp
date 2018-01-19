88e00e90b0370b0748eb5a4cba94894024aabe2b
Vijay Manikpuri <vijaydas.mp@gmail.com>
2018-01-19 20:27:41 +0530

    Update kruskals_minimu_spanning.cpp

--- kruskals_minimu_spanning.cpp
+++ kruskals_minimu_spanning.cpp
@@ -1,1 +1,26 @@
+typedef struct Edge
+{
+   int src;
+   int dest;
+}Edge;
 
+typedef struct Graph
+{
+  int V, E;
+  Edge *edges;
+}Graph;
+
+Graph * CreateGraph(int V, int E)
+{
+  Graph *g = (Graph *) malloc(V * sizeof(Graph));
+  g->V = V;
+  g->E = E;
+  
+  g->edges = (Edge*) malloc (E * sizeof(Edge));
+  
+  return g;
+}
+
+void adddEdge(int u , int v, int wt)
+{
+}
