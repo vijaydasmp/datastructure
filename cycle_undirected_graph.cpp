be6d299cd14cc1c6d1d39af8fe4dadd3ce077a20
Vijay Manikpuri <vijaydas.mp@gmail.com>
2018-01-19 11:34:57 +0530

    Update cycle_undirected_graph.cpp

--- cycle_undirected_graph.cpp
+++ cycle_undirected_graph.cpp
@@ -1,1 +1,27 @@
 
+// Detect Cycle in an Undirected Graph
+
+typdef struct Edge
+{
+    int src, dest;
+}Edge;
+ 
+typdef struct Graph
+{
+  // V-> Number of vertices, E-> Number of edges
+  int V;
+  int E;
+  
+  // graph is represented as an array of edges
+    struct Edge* edge;
+}Graph;
+
+Graph * CreateGraph(int V, int E)
+{
+  Graph *g = (Graph *) malloc (sizeof(Graph));
+  g->V = V;
+  g->E = E;
+  
+  g->edge = (Edge *) malloc (E * sizeof(Edge));
+}
+                    
