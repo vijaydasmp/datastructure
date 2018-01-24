77218a1b02aee5a344471be8ef87d29ed2c9bad1
Vijay Manikpuri <vijaydas.mp@gmail.com>
2018-01-24 10:48:40 +0530

    Update binary_search.cpp

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,1 +1,34 @@
+int bsearch(int *array, int n, int item)
+{
+  // Discard zero array
+    if (n <= 0)
+        return -1;
+ 
+    int low = 0;
+    int high = n-1;      
+     
+  return bsearch_util(array, low, high, item); 
+}
 
+
+int bsearch_util(int array[], int low , int high, int item)
+{
+  mid = (low + (high - low))/2;
+  
+  if ( array [mid] == item)
+  {
+      return mid;
+  }
+  if (array[mid] < item)
+  {
+    low = mid;
+    return bsearch_util(array, low, high, item); 
+  }
+  if (array[mid] > item)
+  {
+    high = mid;
+    return bsearch_util(array, low, high, item); 
+  }
+  
+   return -1;
+}
