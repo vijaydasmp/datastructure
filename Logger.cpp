a24d0a0ae1a12d4c9bc5fa7aaacbf8570b9393e1
Vijay Manikpuri <vijaydas.mp@gmail.com>
2018-01-30 11:08:04 +0530

    Update Logger.cpp

--- Logger.cpp
+++ Logger.cpp
@@ -1,6 +1,10 @@
 // Assuming a Simple Design of having only 1 stream to write to , for e.g only to 1 file or console only
+/*
+The requesters need a well-known object to which to send requests to log. This means a global point of access.
+Since the logging service is a single event source to which multiple listeners can register, there only needs to be one instance.
 
-
+https://www.ibm.com/developerworks/webservices/library/co-single/index.html
+*/
 
 
 class Logger
@@ -23,14 +27,37 @@ class Logger
 	
 	public:
 	
+	static Logger* GetLogger()
+	{
+		if(m_instance == NULL)
+			m_instance = new Logger();
+		return m_instance;
+	}
 	initialize()
 	{
-	   logfile.open("/var/tmp/log.txt",ios::out);	
+	   logfile.open("/var/tmp/log.txt",ios::out||ios::app);	
 	}
 	
-	write(MSGTYPE type, str msg )
+	write(MSGTYPE type, const string &msg )
 	{
-		
+	   switch(type)
+	   {
+		   case INFO:
+			   logfile<<"INFO: "<<msg<<endl;
+		   case WARN:
+			   logfile<<"WARN: "<<msg<<endl;
+		   case DEBUG:
+			   logfile<<"DEBUG: "<<msg<<endl;
+	   }
 	}
 		
+}
+
+int main()
+{
+	Logger *log = Logger::GetLogger();
+	log->initialize();
+	
+	log->write(INFO, "starting program")
+	return 0;
 }
