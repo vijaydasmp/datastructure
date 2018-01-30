6dcd9663915e2d8b141e0011709dde3ba41a5811
Vijay Manikpuri <vijaydas.mp@gmail.com>
2018-01-30 10:18:52 +0530

    Update Logger.cpp

--- Logger.cpp
+++ Logger.cpp
@@ -1,1 +1,23 @@
+// Assuming a Simple Design of having only 1 stream to write to , for e.g only to 1 file or console only
 
+
+
+
+class Logger
+{
+	
+	private:
+	
+	enum class Messagetype
+	{
+	   INFO,
+	   WARN,
+	   DEBUG
+	}
+
+	Logger();
+  Logger(const Logger &lg);
+  Logger& operator =(const Logger &lg);
+  ~Logger();
+	
+}
